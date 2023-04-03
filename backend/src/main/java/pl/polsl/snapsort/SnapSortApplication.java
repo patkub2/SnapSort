78ce6029f78a37ba1344b68b3ba726ac6fe7068b
patrykkubala <patrykkubala1999@gmail.com>
2023-04-03 22:42:11 +0200

    added java spring template

---
+++ backend/src/main/java/pl/polsl/snapsort/SnapSortApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.snapsort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapSortApplication.class, args);
+	}
+
+}
