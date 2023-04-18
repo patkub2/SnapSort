57cd1b1475892423c895c33b1aceddaa7c2c0783
Marcel Filejecki <filej1025@gmail.com>
2023-04-18 18:45:57 +0200

    small changes

---
+++ frontend/src/pages/registration/index.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+import RegistrationForm from "@/components/registration/registration-form";
+
+export default function Registration() {
+  return (
+    <Fragment>
+      <RegistrationForm />
+    </Fragment>
+  );
+}
