56d9fec3baed5aabe141d002b4a9c1ee7cf1d27f
Marcel Filejecki <filej1025@gmail.com>
2023-04-20 17:18:53 +0200

    added styling for login component

---
+++ frontend/src/components/login/login-form.tsx
@@ -0,0 +1,42 @@
+import React, { FC, ReactElement, useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Box,
+  LogoImage,
+  Form,
+  Input,
+  Button,
+  H3,
+  LoginLink,
+} from "../registration/registration-form";
+
+type ChildProps = {};
+
+const LoginForm: FC<ChildProps> = (): ReactElement => {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Box>
+      <LogoImage src="logo.svg" alt="Logo of the page" width={60} height={60} />
+      <Form onSubmit={submitHandler}>
+        <Input type="email" placeholder="Email Address" ref={emailInputRef} />
+        <Input type="password" placeholder="Password" ref={passwordInputRef} />
+        <Button>Log In</Button>
+      </Form>
+      <H3>
+        Need a new account?
+        <LoginLink style={{ textDecoration: "none" }} href={"/registration"}>
+          Register Now
+        </LoginLink>
+      </H3>
+    </Box>
+  );
+};
+
+export default LoginForm;
