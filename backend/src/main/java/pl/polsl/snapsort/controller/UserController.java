fa170baebf592099e04aa50efa4dbfaa94b41b51
patrykkubala <patrykkubala1999@gmail.com>
2023-04-27 18:56:15 +0200

    working user table

--- backend/src/main/java/pl/polsl/snapsort/controller/UserController.java
+++ backend/src/main/java/pl/polsl/snapsort/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.thymeleaf.model.IModel;
 import pl.polsl.snapsort.dto.UserDto;
 import pl.polsl.snapsort.service.UserService;
 
--- backend/src/main/java/pl/polsl/snapsort/repository/UserRepository.java
+++ backend/src/main/java/pl/polsl/snapsort/repository/UserRepository.java
@@ -3,32 +3,9 @@ package pl.polsl.snapsort.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.polsl.snapsort.models.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-    User findByEmail(String email);
-
-    User findByActivationCode(String code);
-
-    User findByResetPasswordCode(String code);
-
-    User findByUsernameAndPassword(String username, String password);
-
-    User findByUsernameAndEmail(String username, String email);
-
-    User findByUsernameAndActivationCode(String username, String code);
-
-    User findByUsernameAndResetPasswordCode(String username, String code);
-
-    User findByUsernameAndPasswordAndActivationCode(String username, String password, String code);
-
-    User findByUsernameAndPasswordAndResetPasswordCode(String username, String password, String code);
-
-    User findByUsernameAndPasswordAndEmail(String username, String password, String email);
-
-    User findByUsernameAndPasswordAndActivationCodeAndResetPasswordCode(String username, String password, String code, String code2);
-
-    User findByUsernameAndPasswordAndEmailAndActivationCode(String username, String password, String email, String code);
-
-    User findByUsernameAndPasswordAndEmailAndResetPasswordCode(String username, String password, String email, String code);
+    Optional<User> findByUsername(String url);
 }
+
