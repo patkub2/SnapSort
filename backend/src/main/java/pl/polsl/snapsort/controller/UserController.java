76cec0a05a9764ed10e99bbea7141bc82c4b8efb
patrykkubala <patrykkubala1999@gmail.com>
2023-05-09 15:57:33 +0200

    Cors temporary fix

--- backend/src/main/java/pl/polsl/snapsort/controller/UserController.java
+++ backend/src/main/java/pl/polsl/snapsort/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import pl.polsl.snapsort.service.UserService;
 import java.util.List;
 
 @Controller
+@CrossOrigin("*")
 public class UserController {
     private UserService userService;
 
