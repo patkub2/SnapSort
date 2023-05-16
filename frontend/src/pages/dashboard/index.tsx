579ed9637e29a06cc86cb1076c4a8b418d292dad
Marcel Filejecki <filej1025@gmail.com>
2023-05-16 02:35:57 +0200

    added dashboard page

---
+++ frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Layout from "@/components/layout/layout";
+
+const DashBoard = () => {
+  return (
+    <Fragment>
+      <Layout />
+    </Fragment>
+  );
+};
+
+export default DashBoard;
