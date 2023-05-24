0c3d3a3b36d168b099c213f5c7f6129e191c06c3
Marcel Filejecki <filej1025@gmail.com>
2023-05-24 20:03:25 +0200

    added NextAuth config file

---
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,44 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials, req) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const res = await fetch("http://localhost:8080/api/auth/login", {
+          method: "POST",
+          body: JSON.stringify({ username: email, password }),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+        if (res.ok && user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/logins",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      session.user = token;
+      return session;
+    },
+  },
+  secret: "2VrAjU7NUS14mDKICeTwEO3S2Gad8jtlHqQobDz69Io=",
+});
