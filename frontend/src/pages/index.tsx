900bb826bf6b3af005d3dd56b56d1ec5cb3d916e
Marcel Filejecki <filej1025@gmail.com>
2023-05-30 20:53:42 +0200

    feat: swapped dashboard to home page and middleware update

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,16 @@
-import { Fragment } from "react";
+import Layout from "@/components/layout/layout";
+import { useSession } from "next-auth/react";
+import { Fragment, useEffect } from "react";
 
 export default function StartPage() {
-  return <Fragment></Fragment>;
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    console.log(session);
+    console.log(status);
+  }, []);
+  return (
+    <Fragment>
+      <Layout />
+    </Fragment>
+  );
 }
