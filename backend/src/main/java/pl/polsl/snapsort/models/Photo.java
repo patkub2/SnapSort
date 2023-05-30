9afed876c2f14a9b3eb433312d9d8ea5c1903b73
Patryk Kubala <patrykkubala1999@gmail.com>
2023-05-30 20:57:17 +0200

    add models

---
+++ backend/src/main/java/pl/polsl/snapsort/models/Photo.java
@@ -0,0 +1,33 @@
+package pl.polsl.snapsort.models;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table (name = "Photo")
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "photo_data_id")
+    private PhotoData photoData;
+
+    @OneToOne (cascade = CascadeType.ALL)
+    @JoinColumn (name = "thumbnail_data_id")
+    private ThumbnailData thumbnailData;
+
+}
---
+++ backend/src/main/java/pl/polsl/snapsort/models/ThumbnailData.java
@@ -0,0 +1,24 @@
+package pl.polsl.snapsort.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table (name = "ThumbnailData")
+public class ThumbnailData {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(name = "data")
+    private byte[] data;
+}
